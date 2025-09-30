1c9465a924a50dd05de811e2535139c2ed55cacc
fanzhang0830 <fanzhang0830@gmail.com>
2025-09-30 18:06:26 -0400

    Add raw data and refine the model

--- src/RF_model.py
+++ src/RF_model.py
@@ -1,3 +1,4 @@
+# This script builds a random forest model to predict chlorophyll-a concentration and saves the trained model in trained_models folder.
 # %%
 # import necessary libraries
 import osgeo
@@ -8,7 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import rasterio
 from rasterio.transform import from_origin
@@ -62,8 +63,25 @@ target_crs = "EPSG:4326"
 # define xarray
 ds = rioxarray.open_rasterio(tiff_file)
 
+s2_image = ds
+B3_B2 = s2_image.sel(band=3) / s2_image.sel(band=2)
+B4_B3 = s2_image.sel(band=4) / s2_image.sel(band=3)
+B5_B4 = s2_image.sel(band=5) / s2_image.sel(band=4)
+ndvi = (s2_image.sel(band=8) - s2_image.sel(band=4)) / (
+    s2_image.sel(band=8) + s2_image.sel(band=4)
+)
+
+# Add the band ratios to the DataArray
+# Create a new DataArray with an additional 'band' dimension for the ratios
+new_bands = xr.concat([B3_B2, B4_B3, B5_B4, ndvi], dim="band")
+new_bands = new_bands.assign_coords(band=[12, 13, 14, 15])  # Assign new band numbers
+
+# Combine the original and new bands into one DataArray
+s2_image = xr.concat([s2_image, new_bands], dim="band")
+
 # reproject the xarray
-reprojected_ds = ds.rio.reproject(target_crs)
+reprojected_ds = s2_image.rio.reproject(target_crs)
+
 
 # import the geometry data of train_test data
 tgt_x = xr.DataArray(chl_df["Longitude"], dims="points")
@@ -71,22 +89,28 @@ tgt_y = xr.DataArray(chl_df["Latitude"], dims="points")
 
 # extract band values at each points
 band_values = reprojected_ds.sel(
-    band=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], x=tgt_x, y=tgt_y, method="nearest"
+    band=[12, 13, 14, 15], x=tgt_x, y=tgt_y, method="nearest"
 ).values
+
 
 # extract ch_in_situ values at each points
 ch_in_situ = chl_df["ch_in_situ"]
 
 # build the random forest model to fit different band values with ch_in_situ
 
-X = pd.DataFrame(band_values.T, columns=reprojected_ds.long_name)
+X = pd.DataFrame(band_values.T, columns=("B3_B2", "B4_B3", "B5_B4", "ndvi"))
 y = ch_in_situ
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=83
 )
 
-rf_regressor = RandomForestRegressor(n_estimators=500, max_depth=10, random_state=83)
+rf_regressor = RandomForestRegressor(n_estimators=500, max_depth=15, random_state=83)
+
+clf = GradientBoostingRegressor(
+    n_estimators=500, learning_rate=0.1, max_depth=5, random_state=53
+).fit(X_train, y_train)
+print("check clf accuarcy: ", clf.score(X_test, y_test))
 
 # Fitting the model on the training data
 rf_regressor.fit(X_train, y_train)
